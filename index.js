d732e2aab8efde37a39bcf916c626b346a463889
Huy Hoàng Phan <39024711+hoangph271@users.noreply.github.com>
2020-06-04 18:38:16 +0700

    Use remote GCP DB

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { MongoClient } = require('mongodb')
 const { createApp } = require('./app')
 
 const PORT = 8080
-const DB_URI = 'mongodb://localhost:27017/?readPreference=primary&ssl=false'
-const DB_NAME = 'testTaichiDb'
+const DB_URI = 'mongodb://tc:open_taichi@35.193.125.249/taichi_db'
+const DB_NAME = 'taichi_db'
 
 ;(async () => {
-  const client = await MongoClient.connect(DB_URI, { useUnifiedTopology: true });
+  const client = await MongoClient.connect(DB_URI, { useUnifiedTopology: true })
   const db = client.db(DB_NAME)
 
   const app = await createApp({ db })
